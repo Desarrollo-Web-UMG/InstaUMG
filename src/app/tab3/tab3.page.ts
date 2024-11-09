f632b78876e72b9357b71c6d39feb62b89c2c712
William <49001552+Andres715@users.noreply.github.com>
2024-11-09 10:22:30 -0600

    Add files via upload

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -30,7 +30,8 @@ export class Tab3Page implements OnInit {
   isEditProfileModalOpen: boolean = false; // Estado para controlar la visibilidad del modal
   selectedImage: string | ArrayBuffer | null = null; // Para almacenar la previsualización de la imagen seleccionada
   isImageSelectorModalOpen: boolean = false; // Controla la visibilidad del modal de selección de imagen
-
+  usuariosUmg: any[] = []; // Array para almacenar la respuesta del API
+  following: any[] = []; // Array para almacenar la respuesta del API de following
 
   constructor(
     private http: HttpClient,
@@ -40,11 +41,15 @@ export class Tab3Page implements OnInit {
   ngOnInit() {
     this.loadUserProfile();
     this.loadUserPosts();
+    this.loadUserList();
+    this.loadFollowing();
   }
 
   ionViewWillEnter() {
     this.loadUserProfile();
     this.loadUserPosts();
+    this.loadUserList();
+    this.loadFollowing();
   }
 
   // Método para cargar el perfil del usuario
@@ -290,6 +295,158 @@ export class Tab3Page implements OnInit {
             await alert.present();
         }
     );
+  }
+
+  loadUserList() {
+    const token = sessionStorage.getItem('token'); // Obtén el token de sessionStorage
+  
+    if (token) {
+      const url = `${this.url}/api/user/list/`;
+      const headers = new HttpHeaders().set('Authorization', token);
+  
+      // Realiza la solicitud HTTP para obtener la lista de usuarios
+      this.http.get(url, { headers }).subscribe(
+        (response: any) => {
+          this.usuariosUmg = response.users;
+          console.log("Lista de usuarios:", this.usuariosUmg, JSON.stringify(this.usuariosUmg));
+        },
+        (error) => {
+          console.error("Error al cargar la lista de usuarios:", error);
+        }
+      );
+    } else {
+      console.error("No se encontró el token en sessionStorage");
+    }
+  }
+  isFollowingModalOpen: boolean = false; // Estado para controlar la visibilidad del modal
+
+  // Método para abrir el modal de Following
+  openFollowingModal() {
+    this.isFollowingModalOpen = true;
+    this.loadUserList(); // Cargar la lista de usuarios cuando se abra el modal
+  }
+
+  // Método para cerrar el modal de Following
+  closeFollowingModal() {
+    this.isFollowingModalOpen = false;
+  }
+
+  // Método para seguir a un usuario
+  followUser(userId: string) {
+    const id_seguidor = sessionStorage.getItem('id'); // Obtén el id del usuario desde sessionStorage
+    const token = sessionStorage.getItem('token'); // Obtén el token de sessionStorage
+
+    if (id_seguidor && token) {
+      const url = `http://localhost:3000/api/follows/follow`;
+      const headers = new HttpHeaders().set('Authorization', token);
+      const body = {
+        id_seguido: userId,
+        id_seguidor: id_seguidor
+      };
+
+      // Realiza la solicitud HTTP para seguir al usuario
+      this.http.post(url, body, { headers }).subscribe(
+        async (response: any) => {
+          console.log("Respuesta de seguimiento:", response);
+          const alert = await this.alertController.create({
+            header: 'Seguimiento',
+            message: 'Has seguido al usuario con éxito.',
+            buttons: ['Aceptar']
+          });
+          await alert.present();
+          this.loadFollowing();
+        },
+        async (error) => {
+          console.error("Error al seguir al usuario:", error);
+          const alert = await this.alertController.create({
+            header: 'Error',
+            message: 'Ocurrió un error al intentar seguir al usuario.',
+            buttons: ['Aceptar']
+          });
+          await alert.present();
+        }
+      );
+    } else {
+      console.error("No se encontró el id o token en sessionStorage");
+    }
+  }
+  
+  // Método para verificar si ya sigue a un usuario
+  isFollowing(userId: string): boolean {
+    return this.following.some(follow => follow.id_seguido === userId);
+  }
+
+  // Método para alternar entre seguir y dejar de seguir
+  toggleFollow(userId: string) {
+    if (this.isFollowing(userId)) {
+      this.unfollowUser(userId);
+    } else {
+      this.followUser(userId);
+    }
+  }
+
+  // Método para dejar de seguir a un usuario
+  unfollowUser(userId: string) {
+    const id_seguidor = sessionStorage.getItem('id'); // Obtén el id del usuario desde sessionStorage
+    const token = sessionStorage.getItem('token'); // Obtén el token de sessionStorage
+
+    if (id_seguidor && token) {
+      const url = `http://localhost:3000/api/follows/unfollow`; // Supón que tienes un endpoint para unfollow
+      const headers = new HttpHeaders().set('Authorization', token);
+      const body = {
+        id_seguido: userId,
+        id_seguidor: id_seguidor
+      };
+
+      // Realiza la solicitud HTTP para dejar de seguir al usuario
+      this.http.post(url, body, { headers }).subscribe(
+        async (response: any) => {
+          console.log("Respuesta de dejar de seguir:", response);
+          const alert = await this.alertController.create({
+            header: 'Unfollow',
+            message: 'Has dejado de seguir al usuario con éxito.',
+            buttons: ['Aceptar']
+          });
+          await alert.present();
+          this.loadFollowing(); // Recargar la lista de following
+        },
+        async (error) => {
+          console.error("Error al dejar de seguir al usuario:", error);
+          const alert = await this.alertController.create({
+            header: 'Error',
+            message: 'Ocurrió un error al intentar dejar de seguir al usuario.',
+            buttons: ['Aceptar']
+          });
+          await alert.present();
+        }
+      );
+    } else {
+      console.error("No se encontró el id o token en sessionStorage");
+    }
+  }
+
+  loadFollowing() {
+    const id_seguidor = sessionStorage.getItem('id'); // Obtén el id del usuario de sessionStorage
+    const token = sessionStorage.getItem('token'); // Obtén el token de sessionStorage
+
+    if (id_seguidor && token) {
+      const url = `${this.url_posts}/api/follows/getFollowsByFollowerId`;
+      const headers = new HttpHeaders().set('Authorization', token);
+      const body = { id_seguidor }; // El cuerpo de la solicitud POST
+
+      // Realiza la solicitud HTTP para obtener la lista de following
+      this.http.post(url, body, { headers }).subscribe(
+        (response: any) => {
+          this.following = response;
+          console.log("Following:", this.following, JSON.stringify(this.following));
+        },
+        (error) => {
+          console.error("Error al cargar los following:", error);
+        }
+      );
+    } else {
+      console.error("No se encontró el id o token en sessionStorage");
+    }
   }
   
 }
